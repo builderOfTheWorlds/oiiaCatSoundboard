2ffce39550dc994adfeddf67c80c23253cc812b9
builderOfTheWorlds <141364273+builderOfTheWorlds@users.noreply.github.com>
2025-02-11 01:30:42 -0500

    Update script.js

--- script.js
+++ script.js
@@ -2,10 +2,18 @@
 // const soundO = new Audio('0_o.wav'); // Replace with the actual path to your sound file
 
 const sounds = {
-  button1: new Audio('0_o.wav'),
-  button2: new Audio('1_i.wav'),
-  button3: new Audio('2_i.wav'),
-  button4: new Audio('3_a.wav'),
+  button1: new Audio('0.wav'),
+  button2: new Audio('1.wav'),
+  button3: new Audio('2.wav'),
+  button4: new Audio('3.wav'),
+  button4: new Audio('4.wav'),
+  button4: new Audio('5.wav'),
+  button4: new Audio('6.wav'),
+  button4: new Audio('7.wav'),
+  button4: new Audio('8.wav'),
+  button4: new Audio('9.wav'),
+  button4: new Audio('10.wav'),
+  button4: new Audio('11.wav')
 };
 
 
