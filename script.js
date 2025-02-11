37dfa2eb43692f2dfea076477830bf5f8965576a
builderOfTheWorlds <141364273+builderOfTheWorlds@users.noreply.github.com>
2025-02-11 01:25:17 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,30 @@
 // Load the sound file
-const soundO = new Audio('0_o.wav'); // Replace with the actual path to your sound file
+// const soundO = new Audio('0_o.wav'); // Replace with the actual path to your sound file
 
-// Add event listener to the button
-document.getElementById('button1').addEventListener('click', () => {
-  soundO.currentTime = 0; // Reset the sound to the beginning
-  soundO.play(); // Play the sound
+const sounds = {
+  button1: new Audio('0_o.wav'),
+  button2: new Audio('1_i.wav'),
+  button3: new Audio('2_i.wav'),
+  button4: new Audio('3_a.wav'),
+};
+
+
+
+
+
+
+// // Add event listener to the button
+// document.getElementById('button1').addEventListener('click', () => {
+//   soundO.currentTime = 0; // Reset the sound to the beginning
+//   soundO.play(); // Play the sound
+// });
+
+
+Object.keys(sounds).forEach((buttonId) => {
+  document.getElementById(buttonId).addEventListener('click', () => {
+    sounds[buttonId].currentTime = 0;
+    sounds[buttonId].play();
+  });
 });
+
+
